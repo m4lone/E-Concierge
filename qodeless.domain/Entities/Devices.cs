a4981ed0a3aa0ffac1a49ca9cb430e4e273486a4
Caique Silva <caique.silva@dlanalytic.com>
2022-12-05 22:58:20 -0300

    Criando entity para Devices

--- qodeless.domain/Entities/Devices.cs
+++ qodeless.domain/Entities/Devices.cs
@@ -6,46 +6,28 @@ using System;
 
 namespace qodeless.domain.Entities
 {
-    public class Device : Entity
+    public class Devices : Entity
     {
-        public string Code { get; set; }
-        public string SerialNumber { get; set; }
-        public EDeviceType Type { get; set; }
-        public EDeviceStatus Status { get; set; }
-        public EDeviceAvailability Availability { get; set; }
-        public Device(Guid id) { Id = id; }
-        public string MacAddress { get; set; }
+        public string DeviceSn { get; set; }
+        public Devices(Guid id) { Id = id; }
+        public Guid? RoomId { get; set; }
 
-        public Device(string code, string serialNumber, EDeviceType type, EDeviceStatus status, EDeviceAvailability availability, string macAddress)
+        public Devices(string deviceSn)
         {
-            Code = code;
-            SerialNumber = serialNumber;
-            Type = type;
-            Status = status;
-            Availability = availability;
-            MacAddress = macAddress;            
+            DeviceSn = deviceSn;   
         }
 
-        //public void Configure(EntityTypeBuilder<Device> builder)
-        //{
-        //    builder.HasKey(c => c.Id);
-        //    builder.Property(x => x.Id).IsRequired();
-        //    builder.Property(x => x.Code).IsRequired();
-        //    builder.Property(x => x.Type).IsRequired();
-        //    builder.Property(x => x.Status).IsRequired();
-        //}
     }
-    public static class DeviceDbBuilder /*Fluent Class*/
+    public static class DevicesDbBuilder /*Fluent Class*/
     {
-        public static EntityTypeBuilder<TEntity> ConfigureUnique<TEntity>(this EntityTypeBuilder<TEntity> self, ModelBuilder modelBuilder) where TEntity : Device
+        public static EntityTypeBuilder<TEntity> ConfigureUnique<TEntity>(this EntityTypeBuilder<TEntity> self, ModelBuilder modelBuilder) where TEntity : Devices
         {
             self.HasKey(c => c.Id);
             self.Property(x => x.Id).IsRequired();
-            self.Property(x => x.Code).IsRequired();
-            self.Property(x => x.Type).IsRequired();
-            self.Property(x => x.Status).IsRequired();
+            self.Property(x => x.DeviceSn).IsRequired();
+            self.Property(x => x.RoomId).IsRequired();
 
-            modelBuilder.Entity<TEntity>().HasIndex(p => new { p.Code,p.SerialNumber,p.MacAddress }).IsUnique();
+            modelBuilder.Entity<TEntity>().HasIndex(p => new { p.DeviceSn}).IsUnique();
             return self;
         }
     }
